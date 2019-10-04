f38f8b85deda8e09d268cef558cb8104a10783e4
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-10-04 10:37:06 +0700

    firebase user authenticate

--- app/src/main/java/com/sinichi/parentingcontrolv3/MainActivity.java
+++ app/src/main/java/com/sinichi/parentingcontrolv3/MainActivity.java
@@ -2,13 +2,31 @@ package com.sinichi.parentingcontrolv3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseUser == null) {
+            // TODO: Activate if login activity exist
+//            startActivity(new Intent(MainActivity.this,
+//                    LoginActivity.class));
+        }
     }
 }

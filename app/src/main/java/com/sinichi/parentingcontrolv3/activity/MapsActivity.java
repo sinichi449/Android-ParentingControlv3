6c81b5297188e749b32ca46054adfc4049be2b7b
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-10-21 10:33:09 +0700

    functioned getCurrent location

--- app/src/main/java/com/sinichi/parentingcontrolv3/activity/MapsActivity.java
+++ app/src/main/java/com/sinichi/parentingcontrolv3/activity/MapsActivity.java
@@ -4,14 +4,20 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sinichi.parentingcontrolv3.R;
 import com.sinichi.parentingcontrolv3.util.Constant;
@@ -22,7 +28,10 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProvider;
+    private LatLng currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,8 +80,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add getCollapsingToolbarLayoutView marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mFusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationProvider.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("Lorem ipsum"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                }
+            }
+        });
     }
 }
